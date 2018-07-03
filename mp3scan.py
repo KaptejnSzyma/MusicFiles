9279246bce19f3492e49629ed1fbe2e1d9174246
Szymek <szymon.rzadca@gmail.com>
2018-07-03 14:07:03 +0200

    handling exceptions in the loop

--- mp3scan.py
+++ mp3scan.py
@@ -1,5 +1,6 @@
 import os
 import fnmatch
+import id3reader_p3 as id3reader
 
 
 def find_music(start, extension):
@@ -9,5 +10,20 @@ def find_music(start, extension):
             yield os.path.join(absolute_path, file)          # use it in yielded values
 
 
-for f in find_music('music', 'emp3'):
-    print(f)
+my_music_files = find_music('music', 'emp3')
+error_list = []
+
+for f in my_music_files:
+    try:
+        id3r = id3reader.Reader(f)
+        print("Artist: {}, Album: {}, Track: {}, Song: {}".format(
+            id3r.get_value('performer'),
+            id3r.get_value('album'),
+            id3r.get_value('track'),
+            id3r.get_value('title')
+        ))
+    except:
+        error_list.append(f)
+
+for error_file in error_list:
+    print(error_file)
