2496cdfc3ef20be32a6a7601e39de735bd8bf98a
Szymek <szymon.rzadca@gmail.com>
2018-07-03 13:46:29 +0200

    adding absolute paths

--- mp3scan.py
+++ mp3scan.py
@@ -5,7 +5,8 @@ import fnmatch
 def find_music(start, extension):
     for path, directiories, files in os.walk(start):
         for file in fnmatch.filter(files, '*.{}'.format(extension)):
-            yield os.path.join(path, file)
+            absolute_path = os.path.abspath(path)   # create absolute path
+            yield os.path.join(absolute_path, file)          # use it in yielded values
 
 
 for f in find_music('music', 'emp3'):
