73437374795a06b8a562b5dc813fd5114b08386c
Szymek <szymon.rzadca@gmail.com>
2018-07-03 13:16:27 +0200

    getting use of function and adding filters, so only the name specified is looked for

--- filesearch.py
+++ filesearch.py
@@ -4,8 +4,14 @@ import fnmatch
 
 def find_albums(root, artist_name):
     for path, directories, files in os.walk(root):
-        for artist in directories:
+        for artist in fnmatch.filter(directories, artist_name):
             subdir = os.path.join(path, artist)
             for album_path, albums, _ in os.walk(subdir):
                 for album in albums:
                     yield os.path.join(album_path, album), album
+
+
+album_list = find_albums("music", "Aerosmith")
+
+for a in album_list:
+    print(a)
