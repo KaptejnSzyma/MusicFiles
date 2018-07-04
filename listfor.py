22346ba44d2523823812c2c08d2898d051cb8327
Szymek <szymon.rzadca@gmail.com>
2018-07-04 13:19:44 +0200

    trying to get a number from user input

--- listfor.py
+++ listfor.py
@@ -2,9 +2,12 @@ print(__file__)
 
 numbers = [1, 2, 3, 4, 5, 6]
 
+number = int(input("Please enter a number and i'll tell you it's square: "))
+
 squares = []
 
 for number in numbers:
     squares.append(number ** 2)
 
-print(squares)
+index_pos = numbers.index(number)
+print(squares[index_pos])
